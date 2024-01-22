ad59a055b99db91a997a8fae102e0f63af94a975
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-22 13:56:28 +0700

    fetch wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import MainContent from '@/components/main/main.slider';
+import { sendRequest } from '@/utils/api';
 import { Container } from '@mui/material';
 import * as React from 'react';
 
-export default function HomePage() {
+export default async function HomePage() {
 
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/tracks/top`,
+    method: 'POST',
+    body: {
+      'category': 'CHILL',
+      'limit': 1
+    },
+  })
   return (
     <Container maxWidth="xl">
       <MainContent />
       <MainContent />
       <MainContent />
-      </Container>
+          </Container>
   );
 }
