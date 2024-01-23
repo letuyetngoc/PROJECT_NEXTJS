d3b28ca5ee3438f87f77e370688d10f311ab5ab8
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-23 15:13:24 +0700

    custom gradient and add time duration

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -2,38 +2,116 @@
 import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { useWavesurfer } from '@wavesurfer/react'
+import { Box, Container } from '@mui/material'
 
 export default function DetailTrackPage() {
     const trackRef = useRef<HTMLDivElement>(null)
+    const durationRef = useRef<HTMLDivElement>(null)
+    const hoverRef = useRef<HTMLDivElement>(null)
+    const timeRef = useRef<HTMLDivElement>(null)
+
     const searchParams = useSearchParams()
     const audio = searchParams.get('audio')
 
-    const gradient: CanvasGradient  = useMemo(() => {
-        const ctx = document.createElement('canvas').getContext('2d')!
-        const gradient =  ctx.createLinearGradient(0, 0, 0, 150)
-        gradient.addColorStop(0, 'rgb(200, 0, 200)')
-        gradient.addColorStop(0.7, 'rgb(100, 0, 100)')
-        gradient.addColorStop(1, 'rgb(0, 0, 0)')
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')!
+
+    // Define the waveform gradient
+    const gradient: CanvasGradient = useMemo(() => {
+        const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1)
+        gradient.addColorStop(0, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1') // Bottom color
+        gradient.addColorStop(1, '#B1B1B1') // Bottom color
         return gradient
+    }, [])
+
+    // Define the progress gradient
+    const progressGradient: CanvasGradient = useMemo(() => {
+        const progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1)
+        progressGradient.addColorStop(0, '#EE772F') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094') // Bottom color
+        progressGradient.addColorStop(1, '#F6B094') // Bottom color
+        return progressGradient
     }, [])
 
     const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
         container: trackRef,
         height: 150,
         waveColor: gradient,
-        progressColor: '#ffa500',
+        progressColor: progressGradient,
         url: `/api?audio=${audio}`,
         barWidth: 2,
     })
+
+    // Hover effect
+    // useEffect(() => {
+    //     const handlePointerMove = (e:any) => {
+    //         hoverRef.current!.style.width = `${e.offsetX}px`
+    //     }
+    //     trackRef.current!.addEventListener('pointermove', handlePointerMove)
+    //     return () => {
+    //         trackRef.current!.removeEventListener('pointermove', handlePointerMove)
+    //     }
+    // }, [])
 
     const onPlayPause = useCallback(() => {
         wavesurfer && wavesurfer.playPause()
     }, [wavesurfer])
 
+    // Current time & duration
+    const formatTime = (seconds: number) => {
+        const minutes = Math.floor(seconds / 60)
+        const secondsRemainder = Math.round(seconds) % 60
+        const paddedSeconds = `0${secondsRemainder}`.slice(-2)
+        return `${minutes}:${paddedSeconds}`
+    }
+
+    useEffect(() => {
+        if (wavesurfer) {
+            wavesurfer!.on('decode', (duration) => (durationRef.current!.textContent = formatTime(duration)))
+            wavesurfer!.on('timeupdate', (currentTime) => (timeRef.current!.textContent = formatTime(currentTime)))
+        }
+    }, [wavesurfer])
+    //
+
     return (
-        <>
-            <div ref={trackRef}>DetailTrackPage</div>
+        <Container maxWidth="xl" sx={{ position: 'relative' }}>
+            <Box ref={trackRef} sx={{
+                ".time": {
+                    position: 'absolute',
+                    zIndex: 11,
+                    top: '50%',
+                    marginTop: '-1px',
+                    transform: 'translateY(-50%)',
+                    fontSize: '11px',
+                    background: 'rgba(0, 0, 0, 0.75)',
+                    padding: '2px',
+                    color: '#ddd',
+                }
+            }}>
+                <Box ref={timeRef} className="time" sx={{ left: 0 }}>0:00</Box>
+                <Box ref={durationRef} className="time" sx={{ right: 0 }}>0:00</Box>
+                {/* <Box ref={hoverRef} sx={{
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    zIndex: 10,
+                    pointerEvents: 'none',
+                    height: '100%',
+                    width: 0,
+                    mixBlendMode: 'overlay',
+                    background: 'rgba(255, 255, 255, 0.5)',
+                    opacity: 0,
+                    transition: 'opacity 0.2s ease',
+                }}></Box> */}
+            </Box>
             <button onClick={onPlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
-        </>
+        </Container>
     )
 }
