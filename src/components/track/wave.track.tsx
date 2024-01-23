4a15fa7da14d07052b3f40f089662ffc6a3d9a0e
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-23 14:25:58 +0700

    use wavesurfer react, add gradient

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,25 +1,39 @@
 'use client'
-import React, { useEffect, useRef } from 'react'
-import WaveSurfer from 'wavesurfer.js'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { useWavesurfer } from '@wavesurfer/react'
 
 export default function DetailTrackPage() {
     const trackRef = useRef<HTMLDivElement>(null)
     const searchParams = useSearchParams()
     const audio = searchParams.get('audio')
-    
-    useEffect(() => {
-        if (trackRef.current) {
-            const wavesurfer = WaveSurfer.create({
-                container: trackRef.current,
-                waveColor: 'rgb(200, 0, 200)',
-                progressColor: 'rgb(100, 0, 100)',
-                // url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/${audio}`,
-                url: `/api?audio=${audio}`,
-            })
-        }
+
+    const gradient: CanvasGradient  = useMemo(() => {
+        const ctx = document.createElement('canvas').getContext('2d')!
+        const gradient =  ctx.createLinearGradient(0, 0, 0, 150)
+        gradient.addColorStop(0, 'rgb(200, 0, 200)')
+        gradient.addColorStop(0.7, 'rgb(100, 0, 100)')
+        gradient.addColorStop(1, 'rgb(0, 0, 0)')
+        return gradient
     }, [])
+
+    const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+        container: trackRef,
+        height: 150,
+        waveColor: gradient,
+        progressColor: '#ffa500',
+        url: `/api?audio=${audio}`,
+        barWidth: 2,
+    })
+
+    const onPlayPause = useCallback(() => {
+        wavesurfer && wavesurfer.playPause()
+    }, [wavesurfer])
+
     return (
-        <div ref={trackRef}>DetailTrackPage</div>
+        <>
+            <div ref={trackRef}>DetailTrackPage</div>
+            <button onClick={onPlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+        </>
     )
 }
