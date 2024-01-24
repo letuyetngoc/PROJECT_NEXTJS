e537683eb2ee0e719b622bbc65386590f59e2725
Ngoc Le <ngocle@rtanalytics.vn>
2024-01-24 17:08:43 +0700

    config session for server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import MainContent from '@/components/main/main.slider';
 import { sendRequest } from '@/utils/api';
 import { Container } from '@mui/material';
+import { getServerSession } from 'next-auth';
 import * as React from 'react';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
 export default async function HomePage() {
+
+  const session = await getServerSession(authOptions)
+  console.log('check session ====>', session)
 
   const chill = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/tracks/top`,
